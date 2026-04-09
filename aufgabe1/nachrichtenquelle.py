c4c27b83482a12f17288fe1b5a0e0c754cf0ab05
HBummele <hannes.bummele@yahoo.de>
2026-04-09 10:33:01 +0200

    shannon fanno

--- aufgabe1/nachrichtenquelle.py
+++ aufgabe1/nachrichtenquelle.py
@@ -75,6 +75,7 @@ class Nachrichtenquelle:
             chars.append(char)
             probs.append(prob)
             infos.append(info)
+
         plt.figure()
         sns.barplot(x=chars, y=probs)
         plt.xlabel("Zeichen")
@@ -90,6 +91,34 @@ class Nachrichtenquelle:
         plt.show()
 
 
+def shannon_fano(symbol_list):
+    if len(symbol_list) <= 1:
+        return
+    total = 0
+    for entry in symbol_list:
+        total += entry[1]
+
+    sum = 0
+
+    index = 0
+    for i in range(len(symbol_list)):
+        sum += symbol_list[i][1]
+        if sum >= total / 2:
+            index = i + 1
+            break
+
+    left = symbol_list[:index]
+    right = symbol_list[index:]
+
+    for item in left:
+        item[2] += "0"
+    for item in right:
+        item[2] += "1"
+
+    shannon_fano(left)
+    shannon_fano(right)
+
+
 if __name__ == "__main__":
     test_wort = "Hochschule"
     quelle1 = Nachrichtenquelle(test_wort)
@@ -98,19 +127,17 @@ if __name__ == "__main__":
     with open("rfc2324.txt", "r", encoding="utf-8") as file:
         text = file.read()
     quelle2 = Nachrichtenquelle(text)
-    prob_list = quelle2.get_sorted_prob_list(False)
-    total_prob = 0
-    for entry in prob_list:
-        total_prob += entry[1]
-    print(total_prob)
 
-    sum = 0
-    half_prob = total_prob / 2
-    for char, prob in total_prob:
+    #Test mit Stuff aus Vorlesung
+    quelle3 = Nachrichtenquelle("HOCHSCHULE")
+    prob_list = quelle3.get_sorted_prob_list(True)
 
-        if sum >= half_prob:
+    symbol_list = []
+    for char, prob in prob_list:
+        symbol_list.append([char, prob, ""])
 
-    sum += prob
+    shannon_fano(symbol_list)
+    print(symbol_list)
 
 
 
