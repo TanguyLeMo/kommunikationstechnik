f0b851f1183a0e761319450defb3b797319d263d
Tanguy Le Morzadec <rataelios@gmail.com>
2026-05-04 17:22:09 +0200

    added block_code

--- aufgabe3/block_code.py
+++ aufgabe3/block_code.py
@@ -16,10 +16,84 @@ class BlockCode:
         self.H = np.concatenate((self.P.T, I_p), axis=1)
         self.S = self._build_syndrome_table()
 
+    def _build_syndrome_table(self):
+        syndrome_table = {}
+        for num_errors in range(1, self.max_corr_bits + 1):
+            for error_positions in combinations(range(self.n), num_errors):
+                error_vector = np.zeros(self.n, dtype=int)
+                for pos in error_positions:
+                    error_vector[pos] = 1
+                syndrome = self.H @ error_vector % 2
+                syndrome_key = int(''.join([str(b) for b in syndrome]), 2) #[0,1,0]->2 [1,0,0]-> 4 [0,0,1]-> 1
+                if syndrome_key not in syndrome_table:
+                    syndrome_table[syndrome_key] = error_vector
+                else:
+                    syndrome_table[syndrome_key] = None
+        return syndrome_table
+
+    def encode(self, message:np.array)->np.array:
+        if len(message) != self.k:
+            raise ValueError(f"dikkah, dein scheiß vektor hat nicht die passende länge K:{self.k}")
+        return message @ self.G % 2
 
 
-    def encode(self, message:str)->str:
-        return ""
-    
-    def decode(self, codeword:str)->tuple[str, int]:
-        return ("",0)
+    def decode(self, codeword:np.array)->tuple[np.array, int]:
+        syndrome = (self.H @ codeword ) % 2
+        syndrome_key = int(''.join([f'{b}' for b in syndrome]),2)
+        if syndrome_key == 0: # passt kein Fehler
+            return codeword[:self.k]
+        error_code = self.S.get(syndrome_key, None)
+        if error_code is None:
+            print("Wallah krise")
+            return None, 0
+        #korrigierbarer Fehler:
+        corrected = (codeword  + error_code) % 2
+        num_errors = int(np.sum(error_code)) #[0100] : 1 -> [1001000] : 2
+        final_message = corrected[:self.k]
+        return final_message, num_errors
+
+
+
+
+def main():
+    # Beispiel P-Matrix (7,4)-Code
+    P = [
+        [1, 1, 0],
+        [1, 0, 1],
+        [0, 1, 1],
+        [1, 1, 1],
+    ]
+
+    bc = BlockCode(P, 1)
+
+    # Zufällige Nachricht
+    message = np.random.randint(0, 2, bc.k)
+    print("Original message:      ", message)
+
+    # Encoding
+    codeword = bc.encode(message)
+    print("Encoded codeword:      ", codeword)
+
+    # Fehler simulieren (1 Bit flip)
+    received = codeword.copy()
+    #error_pos = np.random.randint(0, bc.n)
+    received[6] ^= 1
+    received[3] ^= 1
+
+    print(f"Error at position:     {6},{3}")
+    print("Received codeword:     ", received)
+
+    # Decoding
+    decoded, num_errors = bc.decode(received)
+
+    print("Decoded message:       ", decoded)
+    print("Corrected errors:      ", num_errors)
+
+    # Vergleich
+    if decoded is not None:
+        print("Decoding successful:   ", np.array_equal(message, decoded))
+    else:
+        print("Decoding failed (ambiguous syndrome)")
+
+if __name__ == "__main__":
+    main()
